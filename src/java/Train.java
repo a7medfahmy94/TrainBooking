d32e37dfb3d2e2a7faa38698d44486b0266de4dc
halamohamed199 <hala.mohamed199@gmail.com>
2015-12-27 23:16:20 +0200

    update class train

--- src/java/Train.java
+++ src/java/Train.java
@@ -1,3 +1,12 @@
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,5 +19,29 @@
  */
 public class Train {
     public String name;
-    public int capacity;
+    public Integer id;
+    public Integer capacity;
+    
+    static public ArrayList<Train> getAllTrains() {
+        Connection con = new DBConnection().getConnection();
+        String q = "select * from train;";
+        ResultSet res;
+        try {
+            Statement stmt = con.createStatement();
+            res = stmt.executeQuery(q);
+            ArrayList<Train> ret = new ArrayList<Train>();
+            while(res.next()) {
+                Train t = new Train();
+                t.id = res.getInt("id");
+                t.name = res.getString("name");
+                t.capacity = res.getInt("capacity");
+                ret.add(t);
+            }
+            return ret;
+        } catch (SQLException ex) {
+            Logger.getLogger(Train.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return new ArrayList<Train>();
+    }
 }
